a09e26e301289c2b3d1b10f3d6dadde767197aca
CarolineClark <carolinejaneclark@gmail.com>
2017-04-29 16:27:04 +0200

    Added dandelion prefab

---
+++ Assets/scripts/blow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blow : MonoBehaviour {
+	private bool blown;
+
+	// Use this for initialization
+	void Start () {
+		blown = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		if (!blown) {
+			GetComponent<Animator> ().SetBool ("blowing", true);
+			blown = true;
+		}
+	}
+}
